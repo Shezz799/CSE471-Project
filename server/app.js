7a5bef56df42be5856e05ed510bf400f11d1f7d3
Shezz799 <shehzad.hakim@g.bracu.ac.bd>
2026-03-03 16:31:34 +0600

    feat: setting up initial app and db

---
+++ server/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
